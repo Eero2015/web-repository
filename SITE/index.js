403f575bbbe9a5e718509fa14e53f6d35d8d74ff
IBRAHIM ALIYU ABUBAKAR <eero2015@gmail.com>
2022-06-16 18:52:09 +0000

    passenger counter app but modifications still on progress

--- SITE/index.js
+++ SITE/index.js
@@ -1,6 +1,20 @@
-//document.getElementById("count-el").innerText = 5
-
 let count = 0 
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 function increment() {
-	console.log("the button was clicked")
-}                               
+	count += 1
+	countEl.innerText = count
+
+	  
+}
+
+
+
+function save() {
+	let entries = count + " -"
+	saveEl.textContent += " " + entries
+	countEl.innerText = 0
+	count = 0
+}  
+
+
