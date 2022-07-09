a7e5d118a41ffeb0e5a359dd9de2d23e4c3e74b5
IBRAHIM ALIYU ABUBAKAR <eero2015@gmail.com>
2022-07-09 01:59:16 +0100

    just a continuation of the blackjack game trained by the scrimber.com

--- SITE/index1.js
+++ SITE/index1.js
@@ -1,11 +1,16 @@
-let firstCard = 11
-let secondCard = 8
-let sum = firstCard + secondCard
+let firstCard = 11;
+let secondCard = 10;
+let sum = firstCard + secondCard + 5;
+let hasBlackJack = false;
+let isAlive = true;
 
 if (sum < 21) {
 	console.log("do you want to draw a new card?")
 } else if (sum === 21) {
 	console.log("woohoo! you've got blackjack!")
+	hasBlackJack = true;
 } else if (sum > 21) {
 	console.log("you are out of the game")
-}
+	isAlive = false;
+};
+console.log(isAlive);
